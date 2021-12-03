070d0d76b1938d7f7182ddcdc9f5a28b773182e4
Abdelrahman Fahmy <rooted39@gmail.com>
2021-12-04 01:34:41 +0200

    fixed some texts

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -54,7 +54,7 @@ function Register() {
       <MHidden width="mdDown">
         <SectionStyle>
           <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Manage the job more effectively with Minimal
+            Manage the job more effectively with Nursery-X
           </Typography>
           <img alt="register" src="/static/illustrations/illustration_register.png" />
         </SectionStyle>
